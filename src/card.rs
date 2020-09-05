2eccfac3fb3d365676887c7f05d50a37131bf313
Mattherix <mattherix@protonmail.com>
2020-09-05 22:35:13 +0200

    Add cards

---
+++ src/card.rs
@@ -0,0 +1,33 @@
+// The cards used by the poker game
+
+#[derive(PartialEq)]
+pub enum Suit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+    
+}
+pub struct Value(u8);
+impl Value {
+    pub fn new(number: u8) -> Value {
+        if number < 1 || number > 13 {
+            panic!("Card value must be between 1 and 13");
+        }
+        Value(number)
+    }
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub value: Value
+}
+impl Card {
+    pub fn is_suit(&self, suit: Suit) -> bool {
+        if self.suit == suit {
+            true
+        } else {
+            false
+        }
+    }
+}
